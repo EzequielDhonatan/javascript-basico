225088f5fcf4c9b1c1fa7891efb10e8f2ee6ece1
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-02-19 10:31:59 -0300

    Variáveis JavaScript

--- app.js
+++ app.js
@@ -4,3 +4,55 @@
 console.log('Hellow Word');
 console.log('Hellow Word');
 */
+
+/**
+ * Variáveis
+ */
+var ezequiel = 'Ezequiel Dhonatan';
+
+console.log(ezequiel);
+
+ezequiel = true;
+console.log(ezequiel);
+
+ezequiel = 1999;
+console.log(ezequiel);
+
+ezequiel = 3.14;
+console.log(ezequiel);
+
+/**
+ * ## Tipo de Variáveis
+ * 
+ * # String: 'Sou uma String' (Padrão mais rápido)
+ * # String: "Sou uma String"
+ * 
+ * # Inteiro: 12
+ * 
+ * # Booleanos: true / false
+ * 
+ * # Pontos Flutuantes: 3.14159265
+ * 
+ * # Array: new Array()
+ *         [1,.2,3]
+ * 
+ * # Objetos: var = car {nome: "Fusion", Ano: 2019, Marca: 'Ford'};
+ * 
+ * # Undefined: var teste;
+ */
+
+ /**
+  * ## ERROS
+  * 
+  * # Não Inciar com Números: 123nomeVar = 123;
+  * 
+  * # Não utilizar espaços: nome var = 321;
+  * 
+  * # Não utilizar caracteres especiais: !@#$%¨&*()-~{}[];.<>\: nome$@#Var = 232132;
+  * 
+  * # Utilizar variáveis que criou: area = altura * largura;
+  * 
+  * Ter menos de 15 caracteres: nomeDeVariavelComMuitosCaracteres = 321;
+  * 
+  * # Nome fazer sentido: a = 'Nome Pessoa';
+  */
