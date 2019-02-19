0cac8bdb370d444b343d929d3c15c76249a23d70
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-02-19 19:39:57 -0300

    JavaScript Funções Aninhadas

--- app.js
+++ app.js
@@ -295,3 +295,19 @@ else
 // }
 
 // recursive(number);
+
+
+/* ********************** FUNÇÕES ANINHADAS ************************** */
+// function aninhadas(n1, n2, n3) {
+//   function calc(n) {
+//     return n * n3;
+//   }
+  
+//   return calc(n1) + calc(n2);
+// }
+
+// function dobra(n) {
+//   return n * 2;
+// }
+
+// console.log(aninhadas(2, 4, 4));
