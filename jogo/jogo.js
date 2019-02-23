9cf17da4870937d7551c0b81df4dfaa7a22a53ee
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-02-23 18:27:12 -0300

    Jogo Adivinhe um Número com JavaScript

--- jogo/jogo.js
+++ jogo/jogo.js
@@ -1,3 +1,26 @@
 // GERANDO NÚMEROS ALEATÓRIOS
 var random = Math.floor(Math.random() * 10);
-console.log(random);
+var plays = 1;
+var number = 0;
+
+play();
+
+function play () {
+    number = prompt(plays + 'º Tentativa, advinhe o número:');
+
+    checkResult();
+}
+
+function checkResult() {
+    console.log(number + " == " + random);
+
+    if (number == random) {
+        alert('Acetou. Parabéns!!!');
+    } else if (plays >= 3) {
+            alert('Game Over');
+    } else {
+        plays ++;
+
+        play();
+    }
+}
